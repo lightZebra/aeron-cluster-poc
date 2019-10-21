0ad5edd00a9444702e6bd16e13579e017b28d2f9
vladyslavsavchuk <vladyslav.savchuk@jivygroup.com>
2019-10-21 11:45:34 +0300

    Add docker

--- aeron-cluster-poc-examples/scripts/client-interactive.sh
+++ aeron-cluster-poc-examples/scripts/client-interactive.sh
@@ -9,5 +9,10 @@ echo $JAR_FILE
 
 java \
   -cp target/${JAR_FILE}:target/lib/* \
-  -Daeron.cluster.member.endpoints="0=node0:20110,1=node1:20111,2=node2:20112" \
+  -Daeron.cluster.member.endpoints="0=localhost:20110,1=localhost:20111,2=localhost:20112" \
+  -Daeron.cluster.session.timeout=30000000000 \
+  -Daeron.cluster.leader.heartbeat.timeout=50000000000 \
+  -Daeron.client.liveness.timeout=200000000000 \
+  -Daeron.publication.unblock.timeout=300000000000 \
+  -Daeron.driver.timeout=200000000000 \
   ${JVM_OPTS} io.scalecube.acpoc.InteractiveClient
